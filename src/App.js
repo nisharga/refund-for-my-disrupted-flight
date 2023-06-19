7b79de9d1d8195f3bd579b3576bed0260ac6e348
Nisharga Kabir <kabirnisharga@gmail.com>
2023-06-19 15:05:36 +0600

    [done] tailwind css setup and folder setup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is default app</h1>
+    <div>
+      <h1 className="text-xl bg-gradient-to-r from-pink-500 via-lime-500 via-to-sky-800">
+        This is default app, Tailwind css already setup here
+      </h1>
     </div>
   );
 }
