dc6ba057b8b63af0eec85e035dea7898dd0ce06b
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-06-23 17:48:39 +0200

    pdf and result page added

---
+++ src/Components/Result/Result.jsx
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+import { FaFilePdf } from "react-icons/fa";
+import { jsPDF } from "jspdf";
+
+const Result = () => {
+    const pdfRef = useRef(null);
+    const generatePDF = () => {
+        const content = pdfRef.current;
+        const doc = new jsPDF(
+          {
+            orientation: 'p',
+            unit: 'mm',
+            format: 'a4',
+          }
+        );
+        doc.html(content, {
+          width: 180,
+          windowWidth: 700,
+          margin: [20, 20, 20, 20],
+          html2Canvas: { scale: 0.67 },
+          callback: (doc) => {
+            doc.save('result.pdf')
+          }
+        });
+      }
+    return (
+        <div className='m-5'>
+            <h1 className='text-center text-xl'>Result For Your Disrupted Flight</h1>
+            <div className='flex justify-between'>
+                <div ref={pdfRef}>Result</div>
+                <div>
+                    <button onClick={generatePDF} className="rounded-full flex justify-center items-center gap-1 bg-rose-700 text-white px-3 py-1"><p><FaFilePdf></FaFilePdf></p> <p>PDF</p></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Result;
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import SignIn from "../Components/SignIn/SignIn";
 import SignUp from "../Components/SignUp/SignUp";
 import Main from "../layout/Main";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import Result from "../Components/Result/Result";
 
  export const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ import PrivateRoute from "./PrivateRoute/PrivateRoute";
             {
                 path:"/",
                 element : <PrivateRoute><MultiStepForm/></PrivateRoute>
+            },
+            {
+                path:"/result",
+                element : <PrivateRoute><Result/></PrivateRoute>
             },
             {
                 path:"/login",
