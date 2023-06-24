27b10dc423528857d92a193616c8ea76ef77db4f
sh-mihad <sh.mihad.cse@gmail.com>
2023-06-25 00:55:38 +0600

    try to add input fild on step tow

--- src/Components/MultiStepForm/Step2.jsx
+++ src/Components/MultiStepForm/Step2.jsx
@@ -3,23 +3,28 @@ import React from "react";
 const Step2 = ({ setFormData, formData }) => {
   return (
     <div className="transition-opacity  transform duration-500">
-      <div className="mb-4">
-        <label
-          className="block font-medium mb-2 text-gray-700"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="w-full border border-gray-400 p-2"
-          value={formData?.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-        />
+      <div className="mt-10 bg-[#e8eef1] rounded-lg p-7">
+        <h4 className="font-medium mb-2 text-lg text-gray-700">
+          Do You Have any Recipt Details
+        </h4>
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-3 my-5">
+          <div>
+            <div className="border ">
+              <input
+                type="radio"
+                className="w-4 h-4"
+                id="noReceipt"
+                name="receiptDetails"
+                value="no"
+                checked={formData?.isRecipts === "no"}
+                required
+              />
+              <label htmlFor="noReceipt" className="ml-2 mr-4">
+                No
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
