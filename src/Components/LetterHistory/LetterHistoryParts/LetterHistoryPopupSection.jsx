57df9496c11ed37f63b3229e13204e86e37913c6
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-07-07 11:49:21 +0200

    solve history' page pdf generate

--- src/Components/LetterHistory/LetterHistoryParts/LetterHistoryPopupSection.jsx
+++ src/Components/LetterHistory/LetterHistoryParts/LetterHistoryPopupSection.jsx
@@ -5,51 +5,53 @@ import { toast } from "react-hot-toast";
 import { jsPDF } from "jspdf";
 
 
-const LetterHistoryPopupSection = ({modalRef, handleCloseModal, selectedVal}) => { 
-    const pdfRef = useRef(null);
-    const generatePDF = async () => {
-        if (selectedVal.eligibility) {
-          const content = pdfRef.current;
-          const doc = new jsPDF({
-            orientation: "p",
-            unit: "mm",
-            format: "a4",
-          });
-          await doc.html(content, {
-            width: 180,
-            windowWidth: 700,
-            margin: [20, 20, 20, 20],
-            html2Canvas: { scale: 0.67 },
-            callback: (doc) => {
-              doc.save("result.pdf");
-            },
-          });
-          toast.success("PDF Downloaded Successfully!!")
-        }
-      };
+const LetterHistoryPopupSection = ({ modalRef, handleCloseModal, selectedVal }) => {
+  const pdfRef = useRef(null);
+  const generatePDF = async () => {
+    const content = pdfRef.current;
+    const doc = new jsPDF({
+      orientation: "p",
+      unit: "mm",
+      format: "a4",
+    });
+    await doc.html(content, {
+      width: 180,
+      windowWidth: 700,
+      margin: [20, 20, 20, 20],
+      html2Canvas: { scale: 0.67 },
+      callback: (doc) => {
+        doc.save("letter.pdf");
+      },
+    });
+    toast.success("PDF Downloaded Successfully!!")
+  };
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75">
-    <div className="bg-white w-1/2 rounded-lg" ref={modalRef}>
-      <div className="flex justify-end">
-        <button className="p-2" onClick={handleCloseModal}>
-          <CloseIconSVG />
-        </button>
-      </div>
-        <div className="p-4">
-                <button
-                  onClick={generatePDF}
-                  className="rounded-full flex justify-center items-center gap-1 bg-rose-700 text-white px-3 py-1"
-                >
-                  <p>
-                    <FaFilePdf></FaFilePdf>
-                  </p> 
-                  <p>Save</p>
-                </button> 
-            {selectedVal?.claimLetter.split('\n')
-           .map((paragraph, index) => <span key={index}>{paragraph}<br /></span>)}
+      <div className="bg-white w-1/2 rounded-lg" ref={modalRef}>
+        <div className="flex justify-end">
+          <button className="p-2" onClick={handleCloseModal}>
+            <CloseIconSVG />
+          </button>
         </div>
+        <div className="p-4">
+          <button
+            onClick={generatePDF}
+            className="rounded-full flex justify-center items-center gap-1 bg-rose-700 text-white px-3 py-1"
+          >
+            <p>
+              <FaFilePdf></FaFilePdf>
+            </p>
+            <p>Save</p>
+          </button>
+          {
+            <div ref={pdfRef}>
+              {selectedVal?.claimLetter.split('\n')
+                .map((paragraph, index) => <span key={index}>{paragraph}<br /></span>)}
+            </div>
+          }
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
