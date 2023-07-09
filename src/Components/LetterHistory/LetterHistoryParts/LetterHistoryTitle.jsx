130f1a2f569367783b8f899fa0355854132488fd
sh-mihad <sh.mihad.cse@gmail.com>
2023-07-10 04:25:55 +0600

    step 1 form input fild focus

--- src/Components/LetterHistory/LetterHistoryParts/LetterHistoryTitle.jsx
+++ src/Components/LetterHistory/LetterHistoryParts/LetterHistoryTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LetterHistoryTitle = ({ handleOpenModal, val }) => {
   return (
     <button
-      className="bg-gray-700  hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
+      className="bg-blue-500  hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
       onClick={handleOpenModal}
     >
       View Latter
--- src/Components/LetterHistory/LetterHistoryParts/LetterTableItem.jsx
+++ src/Components/LetterHistory/LetterHistoryParts/LetterTableItem.jsx
@@ -68,7 +68,7 @@ const LetterTableItem = ({ item, setIsOpen, setSelectedVal, ind }) => {
         />
         <button
           onClick={() => handleDelet(item._id)}
-          className="bg-blue-500  hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+          className="bg-rose-700  hover:bg-rose-300 text-white font-bold py-2 px-4 rounded"
         >
           Delete
         </button>
