3a807d4053138c1e1420735d43aa7e348d81aab5
sh-mihad <sh.mihad.cse@gmail.com>
2023-07-10 02:12:24 +0600

    cliem lettar history upateed

--- src/Components/LetterHistory/LetterHistoryParts/LetterHistoryTitle.jsx
+++ src/Components/LetterHistory/LetterHistoryParts/LetterHistoryTitle.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const LetterHistoryTitle = ({handleOpenModal, val}) => {
-  return ( 
+const LetterHistoryTitle = ({ handleOpenModal, val }) => {
+  return (
     <button
-        className="bg-gray-700  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleOpenModal}
-      >
-        Airline: {val?.airlineName}, 
-        FlightNumber: {val?.flightNumber}, 
-        BoardingNumber: {val?.boardingPassNumber} 
-      </button>
-  )
-}
+      className="bg-gray-700  hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
+      onClick={handleOpenModal}
+    >
+      View Latter
+    </button>
+  );
+};
 
-export default LetterHistoryTitle
+export default LetterHistoryTitle;
